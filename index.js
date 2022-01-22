d7d751e71861e501805a4ac16ee17c648b85614f
ercai <ercaid@foxmail.com>
2022-01-22 10:32:00 +0800

    增加断开连接时的房间内提示

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const io = require('socket.io')(3000, {
+  // 配置跨域
   cors: {
-    origin: ['http://localhost:8080']
+    origin: '*',
+    methods: ['GET', 'POST']
   }
 })
 
@@ -18,6 +20,8 @@ io.on('connection', (socket) => {
 
   // 加入房间
   socket.on('join-room', (room, name) => {
+    socket.name = name
+    socket.room = room
     socket.join(room)
     const count = socket.adapter.rooms.get(room).size
     // 发给自己
@@ -36,5 +40,16 @@ io.on('connection', (socket) => {
     }
     // 发给房间里其他人
     socket.to(room).emit('receive-msg', { name: name, count: count, state: 3 })
+  })
+
+  // 断开连接
+  socket.on('disconnect', () => {
+    const roomli = socket.adapter.rooms.get(socket.room)
+    let count
+    if (roomli) {
+      count = roomli.size
+    }
+    // 发给房间里其他人
+    socket.to(socket.room).emit('receive-msg', { name: socket.name, count: count, state: 3 })
   })
 })
